63b9d61fbaf3a642bd925284f32ade7efbea90b3
Mathieu Moreault <seranx@gmail.com>
2022-07-04 15:21:00 -0400

    Added IsWithinRange method

---
+++ OPEX.Tests/IsWithinRange.cs
@@ -0,0 +1,359 @@
+﻿using ToolBX.Eloquentest.Extensions;
+
+namespace OPEX.Tests;
+
+[TestClass]
+public class IsWithinRange
+{
+    [TestClass]
+    public class WithArray : Tester
+    {
+        [TestMethod]
+        public void WhenSourceIsNull_Throw()
+        {
+            //Arrange
+            Dummy[] source = null!;
+            var index = Fixture.Create<int>();
+
+            //Act
+            var action = () => source.IsWithinRange(index);
+
+            //Assert
+            action.Should().Throw<ArgumentNullException>().WithParameterName(nameof(source));
+        }
+
+        [TestMethod]
+        public void WhenIndexIsZero_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToArray();
+            var index = 0;
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsEqualToLastIndex_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToArray();
+            var index = source.LastIndex();
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsBetweenZeroAndLastIndex_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToArray();
+            var index = Fixture.CreateBetween(0, source.LastIndex());
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsNegative_ReturnFalse()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToArray();
+            var index = -Fixture.Create<int>();
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsGreaterThanLastIndex_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToArray();
+            var index = source.LastIndex() + 1;
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+    }
+
+    [TestClass]
+    public class WithList : Tester
+    {
+        [TestMethod]
+        public void WhenSourceIsNull_Throw()
+        {
+            //Arrange
+            List<Dummy> source = null!;
+            var index = Fixture.Create<int>();
+
+            //Act
+            var action = () => source.IsWithinRange(index);
+
+            //Assert
+            action.Should().Throw<ArgumentNullException>().WithParameterName(nameof(source));
+        }
+
+        [TestMethod]
+        public void WhenIndexIsZero_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToList();
+            var index = 0;
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsEqualToLastIndex_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToList();
+            var index = source.LastIndex();
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsBetweenZeroAndLastIndex_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToList();
+            var index = Fixture.CreateBetween(0, source.LastIndex());
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsNegative_ReturnFalse()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToList();
+            var index = -Fixture.Create<int>();
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsGreaterThanLastIndex_ReturnFalse()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToList();
+            var index = source.LastIndex() + 1;
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+    }
+
+    [TestClass]
+    public class WithReadOnlyList : Tester
+    {
+        [TestMethod]
+        public void WhenSourceIsNull_Throw()
+        {
+            //Arrange
+            IReadOnlyList<Dummy> source = null!;
+            var index = Fixture.Create<int>();
+
+            //Act
+            var action = () => source.IsWithinRange(index);
+
+            //Assert
+            action.Should().Throw<ArgumentNullException>().WithParameterName(nameof(source));
+        }
+
+        [TestMethod]
+        public void WhenIndexIsZero_ReturnTrue()
+        {
+            //Arrange
+            IReadOnlyList<Dummy> source = Fixture.CreateMany<Dummy>().ToList();
+            var index = 0;
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsEqualToLastIndex_ReturnTrue()
+        {
+            //Arrange
+            IReadOnlyList<Dummy> source = Fixture.CreateMany<Dummy>().ToList();
+            var index = source.LastIndex();
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsBetweenZeroAndLastIndex_ReturnTrue()
+        {
+            //Arrange
+            IReadOnlyList<Dummy> source = Fixture.CreateMany<Dummy>().ToList();
+            var index = Fixture.CreateBetween(0, source.LastIndex());
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsNegative_ReturnFalse()
+        {
+            //Arrange
+            IReadOnlyList<Dummy> source = Fixture.CreateMany<Dummy>().ToList();
+            var index = -Fixture.Create<int>();
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsGreaterThanLastIndex_ReturnFalse()
+        {
+            //Arrange
+            IReadOnlyList<Dummy> source = Fixture.CreateMany<Dummy>().ToList();
+            var index = source.LastIndex() + 1;
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+    }
+
+    [TestClass]
+    public class WithWriteOnlyList : Tester
+    {
+        [TestMethod]
+        public void WhenSourceIsNull_Throw()
+        {
+            //Arrange
+            WriteOnlyList<Dummy> source = null!;
+            var index = Fixture.Create<int>();
+
+            //Act
+            var action = () => source.IsWithinRange(index);
+
+            //Assert
+            action.Should().Throw<ArgumentNullException>().WithParameterName(nameof(source));
+        }
+
+        [TestMethod]
+        public void WhenIndexIsZero_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToWriteOnlyList();
+            var index = 0;
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsEqualToLastIndex_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToWriteOnlyList();
+            var index = source.LastIndex();
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsBetweenZeroAndLastIndex_ReturnTrue()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToWriteOnlyList();
+            var index = Fixture.CreateBetween(0, source.LastIndex());
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsNegative_ReturnFalse()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToWriteOnlyList();
+            var index = -Fixture.Create<int>();
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void WhenIndexIsGreaterThanLastIndex_ReturnFalse()
+        {
+            //Arrange
+            var source = Fixture.CreateMany<Dummy>().ToWriteOnlyList();
+            var index = source.LastIndex() + 1;
+
+            //Act
+            var result = source.IsWithinRange(index);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+    }
+}
---
+++ OPEX/IsWithinRange.cs
@@ -0,0 +1,32 @@
+﻿namespace ToolBX.OPEX;
+
+public static partial class CollectionExtensions
+{
+    /// <summary>
+    /// Whether or not the index is between 0 and source's last index.
+    /// </summary>
+    public static bool IsWithinRange<T>(this T[] source, int index) => (source as IList<T>).IsWithinRange(index);
+
+    /// <summary>
+    /// Whether or not the index is between 0 and source's last index.
+    /// </summary>
+    public static bool IsWithinRange<T>(this List<T> source, int index) => (source as IList<T>).IsWithinRange(index);
+
+    /// <summary>
+    /// Whether or not the index is between 0 and source's last index.
+    /// </summary>
+    public static bool IsWithinRange<T>(this IList<T> source, int index)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        return index >= 0 && index <= source.LastIndex();
+    }
+
+    /// <summary>
+    /// Whether or not the index is between 0 and source's last index.
+    /// </summary>
+    public static bool IsWithinRange<T>(this IReadOnlyList<T> source, int index)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        return index >= 0 && index <= source.LastIndex();
+    }
+}
