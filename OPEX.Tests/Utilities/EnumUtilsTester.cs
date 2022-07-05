330fcceaefcbd1c0d8c96e9f169a4af66e6f1f8f
Mathieu Moreault <seranx@gmail.com>
2022-07-05 06:53:48 -0400

    Added EnumUtils to convert enums to enumerables

---
+++ OPEX.Tests/Utilities/EnumUtilsTester.cs
@@ -0,0 +1,37 @@
+﻿using ToolBX.OPEX.Utilities;
+
+namespace OPEX.Tests.Utilities;
+
+[TestClass]
+public class EnumUtilsTester
+{
+    public enum DummyEnum
+    {
+        One,
+        Two,
+        Three
+    }
+
+    [TestClass]
+    public class ToList : Tester
+    {
+        [TestMethod]
+        public void Always_ReturnListWithAllValuesOfTheEnum()
+        {
+            //Arrange
+            var expected = new List<DummyEnum>
+            {
+                DummyEnum.One,
+                DummyEnum.Two,
+                DummyEnum.Three
+            };
+
+            //Act
+            var result = EnumUtils.ToList<DummyEnum>();
+
+            //Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+
+}
---
+++ OPEX/Utilities/EnumUtils.cs
@@ -0,0 +1,6 @@
+﻿namespace ToolBX.OPEX.Utilities;
+
+public static class EnumUtils
+{
+    public static IReadOnlyList<T> ToList<T>() where T : Enum => ((T[])Enum.GetValues(typeof(T))).ToList();
+}
