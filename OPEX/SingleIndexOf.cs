437294617b085ab998fb9df34febbea8228ccdf5
Mathieu Moreault <seranx@gmail.com>
2024-01-07 14:13:23 -0500

    Changed publishing script, updated nugets and removed ReadOnlyListExtensions from ToolBX.Collections

--- OPEX/SingleIndexOf.cs
+++ OPEX/SingleIndexOf.cs
@@ -13,22 +13,5 @@ public static partial class CollectionExtensions
     public static int SingleIndexOf<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> match)
     {
         return source.IndexesOf(match).Single();
-
-        if (source == null) throw new ArgumentNullException(nameof(source));
-        if (match == null) throw new ArgumentNullException(nameof(match));
-        var list = source as IList<TSource> ?? source.ToArray();
-
-        var isFound = false;
-        var index = -1;
-        for (var i = 0; i < list.Count; i++)
-            if (match(list[i]))
-            {
-                if (isFound) throw new InvalidOperationException("Sequence contains more than one matching element");
-                isFound = true;
-                index = i;
-            }
-
-        if (index < 0) throw new InvalidOperationException("Sequence contains no matching element");
-        return index;
     }
 }
